91ed0fb3eeb3f5a8d9f880e58448711224639ab2
Влад Егоров <vladxd228@gmail.com>
2018-07-04 09:29:22 +0300

    commit

--- bin/run.js
+++ bin/run.js
@@ -1,2 +1,9 @@
 require('./../lib/commands');
-require('./../lib/resend_observer')
+require('./../lib/resend_observer');
+const cron = require("node-cron");
+const { TestFunc } = require('./../lib/commands/help');
+
+// cron.schedule("10 * * * * *", function() {
+//   TestFunc();
+// });
+
